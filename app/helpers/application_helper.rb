2dd2fea5e2b51b4a0a914be72b9e4ccfe7520a66
Arena <arena@visualarena.com>
2015-12-27 18:19:10 -0800

    changed base page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "#{@city.abbreviation}72"
+    base_title = "SJFD72"
     if page_title.empty?
       base_title
     else
@@ -12,5 +12,5 @@ module ApplicationHelper
   def city_residents
     @city.moniker.blank? ? "#{@city.name} residents" : @city.moniker
   end
-  
+
 end
